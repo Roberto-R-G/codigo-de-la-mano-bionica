d93a968dd9d45689e87efa75d8013cd325e5375c
Roberto Rodriguez <roberto_rg94@yahoo.com>
2026-04-19 21:36:18 +0200

    Update Brillo_LED_ESP32.ino

--- Brillo_LED_ESP32/Brillo_LED_ESP32.ino
+++ Brillo_LED_ESP32/Brillo_LED_ESP32.ino
@@ -1,1 +1,20 @@
+const int ledPin = 5;
 
+int brightness = 0;
+int step = 5;
+
+void setup() {
+  ledcAttach(ledPin, 5000, 8);
+}
+
+void loop() {
+  ledcWrite(ledPin, brightness);
+
+  brightness += step;
+
+  if (brightness <= 0 || brightness >= 255) {
+    step = -step;
+  }
+
+  delay(20);
+}
